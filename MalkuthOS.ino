caeea7f7ac7c1b7e041ed269b589a9af1402760e
Paul Merrill <pdm@pdm.me>
2019-11-22 11:47:48 -0800

    Compile LispLibrary from lisp/*.lisp
    Remove the fixed LispLibrary variable from MalkuthOS.ino and add a
    script which can be used to create the variable at compile time from the
    content of the lisp/*.lisp files.
    
    Before the first build and upon any subsequent changes to lisp/*.lisp
    files, tools/compile-lisp.sh must be run. This tool will create a lisp.h
    and lisp.cpp file with the content of lisp/*.lisp, so a recompile of the
    C sources should then be done before re-upload.

--- MalkuthOS.ino
+++ MalkuthOS.ino
@@ -19,18 +19,7 @@
 */
 
 // Lisp Library
-const char LispLibrary[] PROGMEM =
-    "(defun ps (&rest args) "
-      "(let ((pstemp (pslist))) "
-        "(if (zerop (length args)) "
-          "pstemp "
-          "(dolist (pn pstemp) "
-            "(if (stringp (first args)) "
-              "(if (string= (third pn) (first args)) "
-                "(return pn))) "
-            "(if (numberp (first args)) "
-              "(if (= (first pn) (first args)) "
-                "(return pn)))))))"; // ps function
+#include "lisp.h"
 
 // Compile options
 
