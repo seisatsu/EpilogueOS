c87f758866d7d424eeb7808f2c19a1885246bcbe
Paul Merrill <pdm@pdm.me>
2021-12-21 14:32:41 -0800

    vector: Add comments

--- Vector.h
+++ Vector.h
@@ -7,11 +7,25 @@
 #define xmalloc(T, count) \
     ((T*)malloc(sizeof(T) * (count)))
 
+/**
+ * A dynamic array of contiguous memory, like std::vector.
+ */
 template<typename X>
 class mal_vector {
  public:
+    /**
+     * Pointer to first element.
+     */
     X* data;
+
+    /**
+     * Number of elements currently in the vector.
+     */
     size_t size;
+
+    /**
+     * Number of elements for which memory is reserved.
+     */
     size_t capacity;
 
  public:
@@ -19,8 +33,14 @@ class mal_vector {
     // Constructors & destructor
     //
 
+    /**
+     * Construct an empty vector.
+     */
     mal_vector() : data(0), size(0), capacity(0) { }
 
+    /**
+     * Construct a copy of another vector.
+     */
     mal_vector(const mal_vector& other) {
         if (other.capacity == 0) {
             data = 0;
@@ -36,6 +56,9 @@ class mal_vector {
         }
     }
 
+    /**
+     * Construct vector by stealing another's contents.
+     */
     mal_vector(mal_vector&& other) {
         data = other.data;
         size = other.size;
@@ -44,6 +67,9 @@ class mal_vector {
         other.size = other.capacity = 0;
     }
 
+    /**
+     * Free a vector's memory and destroy its contents.
+     */
     ~mal_vector() {
         for (size_t i = 0; i < size; i++) {
             data[i].~X();
@@ -55,6 +81,9 @@ class mal_vector {
     // Operators
     //
 
+    /**
+     * Destroy the current contents and copy in another vector's.
+     */
     void operator=(const mal_vector& other) {
         for (size_t i = 0; i < size; i++) {
             data[i].~X();
@@ -74,6 +103,9 @@ class mal_vector {
         }
     }
 
+    /**
+     * Destroy the current contents and move in another vector's.
+     */
     void operator=(mal_vector&& other) {
         for (size_t i = 0; i < size; i++) {
             data[i].~X();
@@ -90,15 +122,28 @@ class mal_vector {
     // Element accesses
     //
 
+    /**
+     * Access a single element.
+     */
     X& operator[](size_t i) {
         assert(i < size);
         return data[i];
     }
 
+    /**
+     * Get a pointer to the first element.
+     *
+     * Useful mostly in range-for loops.
+     */
     X* begin() {
         return data;
     }
 
+    /**
+     * Get a pointer past the last element.
+     *
+     * Useful mostly in range-for loops.
+     */
     X* end() {
         return data + size;
     }
@@ -107,18 +152,27 @@ class mal_vector {
     // Element additions
     //
 
+    /**
+     * Add a element to the end by copying an existing value.
+     */
     void push(const X& x) {
         grow();
         new (data + size) X(x);
         size++;
     }
 
+    /**
+     * Add a element to the end by moving an existing value.
+     */
     void push(X&& x) {
         grow();
         new (data + size) X(static_cast<X&&>(x));
         size++;
     }
 
+    /**
+     * Add a element to an arbitrary position by copying an existing value.
+     */
     void insert(size_t i, const X& x) {
         // FIXME: Does not call move constructors.
         assert(i <= size);
@@ -128,6 +182,9 @@ class mal_vector {
         size++;
     }
 
+    /**
+     * Add a element to an arbitrary position by moving an existing value.
+     */
     void insert(size_t i, X&& x) {
         // FIXME: Does not call move constructors.
         assert(i <= size);
@@ -137,6 +194,9 @@ class mal_vector {
         size++;
     }
 
+    /**
+     * Add an array of element to the end by copying existing values.
+     */
     void append(size_t n, const X* xs) {
         assert(n <= size);
         reserve(size + n);  // FIXME: Choose better size.
@@ -151,12 +211,18 @@ class mal_vector {
     // Element removals
     //
 
+    /**
+     * Remove the last element.
+     */
     void pop() {
         assert(size);
         data[size - 1].~X();
         size--;
     }
 
+    /**
+     * Remove an element.
+     */
     void erase(size_t i) {
         assert(i < size);
         for (size_t j = i; j < size - 1; j++) {
@@ -170,8 +236,13 @@ class mal_vector {
     // Miscellaneous
     //
 
-    // Calls move constructors (which empties the old objects), but not call
-    // destructors on the just-moved objects since they are hopefully empty.
+    /**
+     * Allocate uninitialized memory for a number of elements.
+     *
+     * Calls move constructors (which empties the old objects), but does not
+     * call destructors on the just-moved objects since they are hopefully
+     * empty.
+     */
     void reserve(size_t n) {
         assert(n > capacity);
         X* newData = xmalloc(X, n);
@@ -183,6 +254,12 @@ class mal_vector {
         capacity = n;
     }
 
+    /**
+     * Grow or shrink the size of the vector, constructing and destructing
+     * elements as necessary.
+     *
+     * If necessary, additional memory will be allocated.
+     */
     void resize(size_t n) {
         if (n > capacity) {
             reserve(n);
@@ -200,12 +277,18 @@ class mal_vector {
         size = n;
     }
 
+    /**
+     * Grow the vector if it is full.
+     */
     void grow() {
         if (size == capacity) {
             reserve(size == 0 ? 4 : size * 2);  // FIXME: Choose better size.
         }
     }
 
+    /**
+     * Resize the vector to 0 elements.
+     */
     void clear() {
         for (size_t i = 0; i < size; i++) {
             data[i].~X();
@@ -213,6 +296,12 @@ class mal_vector {
         size = 0;
     }
 
+    /**
+     * Drop the vector's data pointer so it won't be freed upon vector
+     * destruction.
+     *
+     * Resets the vector to its default, empty state.
+     */
     void reset() {
         data = 0;
         size = capacity = 0;
